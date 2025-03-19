13382924b1c76d27e595882e592bf56c62699070
deep misra <deepakmishra93152@gmail.com>
2025-03-19 20:00:00 +0530

    MY FIRST STRING PROGRAMME

---
+++ CountCharacter.java
@@ -0,0 +1,14 @@
+public class CountCharacter {
+    public static void main(String[] args) {
+          System.out.println("COUNTIG THE TOTAL NUMBER OF CHRARATER INTO THE STRING");
+          String s = "Deeepk";
+          System.out.println(s);
+          int Count = 0;
+          for( int i = 0 ; i < s.length() ; i++ ){
+          Count++;
+          }
+          System.out.println("TOTAL NUMBER OF CHARACTER");
+          System.out.println(Count);
+        }   
+
+}
